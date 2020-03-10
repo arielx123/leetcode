a0573fc59956ae3edb40e6edfe3fa2a2fea38a68
arielx123 <57654663+arielx123@users.noreply.github.com>
2020-03-09 21:52:48 -0700

    Update intervalssort.java

--- src/intervalssort.java
+++ src/intervalssort.java
@@ -26,3 +26,46 @@ class Solution {
 
     }
 }
+
+
+253. meeting rooms
+
+    public int minMeetingRooms(int[][] intervals) {
+        // Check for the base case. If there are no intervals, return 0
+        if (intervals.length == 0) {
+          return 0;
+        }
+        //min heap
+        PriorityQueue<int[]> heap = new PriorityQueue<int[]>((o1,o2) -> o1[1]- o2[1]);
+        //sort the array by its start time
+        Arrays.sort(
+            intervals,
+            new Comparator<int[]>() {
+                public int compare(int[]a, int[]b){
+                   return a[0] - b[0];
+                }
+            }       
+        );
+        // start with the first meeting, put it to a meeting room
+        heap.offer(intervals[0]);
+    
+        for (int i = 1; i < intervals.length; i++) {
+            // get the meeting room that finishes earliest
+            int[] interval = heap.poll();
+
+            if (intervals[i][0] >= interval[1]) {
+                // if the current meeting starts right after 
+                // there's no need for a new room, merge the interval
+                interval[1]= intervals[i][1];
+            } else {
+                // otherwise, this meeting needs a new room
+                heap.offer(intervals[i]);
+            }
+
+            // don't forget to put the meeting room back
+            heap.offer(interval);
+        }
+
+        return heap.size();
+    }
+
